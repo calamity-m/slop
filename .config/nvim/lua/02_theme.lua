404243a1f05eb71a2d4a88a6b60dfba9de487e27
calamity-m <11038569+calamity-m@users.noreply.github.com>
2026-04-11 16:20:11 +1000

    feat(nvim): reduce themes

--- .config/nvim/lua/02_theme.lua
+++ .config/nvim/lua/02_theme.lua
@@ -2,11 +2,7 @@ vim.pack.add({
 	{ src = "https://github.com/zaldih/themery.nvim" },
 	{ src = "https://github.com/rebelot/kanagawa.nvim" },
 	{ src = "https://github.com/webhooked/kanso.nvim" },
-	{ src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
 	{ src = "https://github.com/sainnhe/gruvbox-material" },
-	{ src = "https://github.com/savq/melange-nvim" },
-	{ src = "https://github.com/oskarnurm/koda.nvim" },
-	{ src = "https://github.com/nikolvs/vim-sunbather" },
 }, { confirm = false })
 
 require("kanagawa").setup({
@@ -27,9 +23,6 @@ themery.setup({
 	themes = {
 		{ name = "Kanagawa Dragon", colorscheme = "kanagawa-dragon" },
 		{ name = "Kanagawa Wave", colorscheme = "kanagawa-wave" },
-
-		{ name = "Rose Pine Moon", colorscheme = "rose-pine-moon" },
-
 		{
 			name = "Gruvbox Material (Soft)",
 			colorscheme = "gruvbox-material",
@@ -54,12 +47,8 @@ themery.setup({
         vim.g.gruvbox_material_better_performance = true
       ]],
 		},
-		{ name = "Melange", colorscheme = "melange" },
-		{ name = "Kanso Zen", colorscheme = "kanso-zen" },
 		{ name = "Kanso Ink", colorscheme = "kanso-ink" },
 		{ name = "Kanso Mist", colorscheme = "kanso-mist" },
-		{ name = "Koda", colorscheme = "koda" },
-		{ name = "Sunbather", colorscheme = "sunbather" },
 	},
 	livePreview = true,
 })
