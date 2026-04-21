1b0a55de090ef25a3dd66156eefccd12e8e8c806
calamity-m <11038569+calamity-m@users.noreply.github.com>
2026-04-21 21:15:49 +1000

    feat(eza): add eza aliases and snippets
    Add .bashrc.d/eza.sh with ll, la, and tree aliases. Add eza.md snippet
    for long list with sort picker.

---
+++ .bashrc.d/eza.sh
@@ -0,0 +1,8 @@
+# Long format with headers and icons
+alias ll='eza -l --header --icons'
+
+# Long format including hidden files
+alias la='eza -la --header --icons'
+
+# Tree view shortcut
+alias tree='eza --tree'
---
+++ .config/peanutbutter/snippets/files/eza.md
@@ -0,0 +1,7 @@
+# Eza Snippets
+
+## eza long list, hidden files, sorted
+
+```
+eza -lha -s <@sort:echo "name\nName\nsize\nextension\nExtension\nmodified\nchanged\naccessed\ncreated\ninode\ntype\nnone">
+```
