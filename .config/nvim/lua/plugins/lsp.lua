416935bcdaaa7c89e94c984640cbaa467dbd1d64
calamity-m <11038569+calamity-m@users.noreply.github.com>
2026-05-02 13:51:19 +1000

    feat(nvim): add lsp inlay hints

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,8 +3,21 @@ local mason = require("plugins.mason")
 
 vim.lsp.enable(mason.lsp_servers)
 
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+		if client and client:supports_method("textDocument/inlayHint") then
+			vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+		end
+	end,
+})
+
 map("n", "<leader>gg", vim.lsp.buf.definition, { desc = "Goto Definition" })
 map("n", "<leader>gh", vim.lsp.buf.hover, { desc = "Hover Documentation" })
+map("n", "<leader>th", function()
+	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }), { bufnr = 0 })
+end, { desc = "Toggle inlay hints" })
 
 vim.filetype.add({
 	filename = {
@@ -21,6 +34,51 @@ vim.lsp.config("lua_ls", {
 		Lua = {
 			diagnostics = {
 				globals = { "vim" },
+			},
+		},
+	},
+})
+
+vim.lsp.config("gopls", {
+	settings = {
+		gopls = {
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				compositeLiteralTypes = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
+			},
+		},
+	},
+})
+
+vim.lsp.config("rust_analyzer", {
+	settings = {
+		["rust-analyzer"] = {
+			inlayHints = {
+				typeHints = {
+					enable = true,
+				},
+				parameterHints = {
+					enable = true,
+				},
+				chainingHints = {
+					enable = true,
+				},
+			},
+		},
+	},
+})
+
+vim.lsp.config("ty", {
+	settings = {
+		ty = {
+			inlayHints = {
+				variableTypes = true,
+				callArgumentNames = true,
 			},
 		},
 	},
