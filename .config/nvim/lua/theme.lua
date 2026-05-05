936e5e33c2f5cfdad38df7ba78ba8226c488d8da
calamity-m <11038569+calamity-m@users.noreply.github.com>
2026-05-06 07:39:16 +1000

    fix(nvim): clear kanagawa float backgrounds

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -35,6 +35,18 @@ require("kanagawa").setup({
 			},
 		},
 	},
+	overrides = function(colors)
+		local theme = colors.theme
+
+		return {
+			NormalFloat = { bg = "none" },
+			FloatBorder = { bg = "none" },
+			FloatTitle = { bg = "none" },
+			NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+			LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+			MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+		}
+	end,
 })
 
 vim.g.gruvbox_material_better_performance = true
