afa4e1ff3865f681606da2ec3d229fb79b1985a9
calamity-m <11038569+calamity-m@users.noreply.github.com>
2026-05-16 13:45:12 +1000

    feat(nvim): better frontmatter rendering in md

--- .config/nvim/lua/plugins/markview.lua
+++ .config/nvim/lua/plugins/markview.lua
@@ -3,3 +3,11 @@ require("markview").setup({
 		icon_provider = "devicons",
 	},
 })
+
+-- Dim the --- frontmatter delimiters (captured as @keyword.directive in markdown)
+vim.api.nvim_set_hl(0, "@keyword.directive.markdown", { link = "Comment" })
+vim.api.nvim_create_autocmd("ColorScheme", {
+	callback = function()
+		vim.api.nvim_set_hl(0, "@keyword.directive.markdown", { link = "Comment" })
+	end,
+})
